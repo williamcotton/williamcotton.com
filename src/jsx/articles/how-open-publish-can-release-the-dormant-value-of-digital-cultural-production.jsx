1d1e19f90fdd84c71f1e6619893bdcece3a4ebdf
William Cotton <williamcotton@gmail.com>
2016-03-07 08:59:39 -0800

    updates

--- src/jsx/articles/how-open-publish-can-release-the-dormant-value-of-digital-cultural-production.jsx
+++ src/jsx/articles/how-open-publish-can-release-the-dormant-value-of-digital-cultural-production.jsx
@@ -3,7 +3,7 @@
 var React = require('react')
 var ReactBootstrap = require('react-bootstrap')
 var Table = ReactBootstrap.Table
-var Button = ReactBootstrap.Button
+var Label = ReactBootstrap.Label
 var Article = require('./article.jsx')
 
 class HowOpenPublishCanReleaseTheDormantValueOfDigitalCulturalProduction extends React.Component {
@@ -15,21 +15,27 @@ class HowOpenPublishCanReleaseTheDormantValueOfDigitalCulturalProduction extends
 
       <p>In his essay <a href='http://www.imf.org/external/pubs/ft/fandd/2001/03/desoto.htm'>The Mystery of Capital</a>, economist Hernando de Soto lays out how the "formal property systems of the West produce six effects that allow their citizens to generate capital."</p>
 
-      <p>Open Publish adheres to the same formal property systems described by de Soto, but pertains specifically to digital media.</p>
+      <p><Label bsStyle='warning'>Already Bored? Scroll down to the Fun Stuff!!</Label></p>
 
-      <p><i>(1) Fixing the economic potential of assets.</i> "Capital is born by representing in writing" and likewise using Open Publish begins with the registration process. What's important is the useful qualities <i>about</i> the digital media that is being registered, not the digital media itself. The name, the file size, and most importantly, the digital fingerprint that represents the data that constitutes the digital media. By focusing on the formal representation rather than the media itself, we have entered "into the conceptual universe where capital lives."</p>
+      <p>In this essay I'd like to show how Open Publish adheres to the same formal property systems described by de Soto, but with specific reference to digital media. Open Publish inherits these formal properties from the Bitcoin blockchain.</p>
 
-      <p><i>(2) Integrating dispersed information into one system.</i> Open Publish registrations and transactions are embedded in a shared, singular and public data store. There is a single consensus, maintained in a decentralized manner, built directly into the mechanics of the Bitcoin blockchain. With this unified formal property system "where property information is standardized and universally available, what owners can do with their assets benefits from the collective imagination of a larger network of people."</p>
+      <p><i>(1) Fixing the economic potential of assets.</i> "Capital is born by representing in writing" and likewise <b>using Open Publish begins with the registration process</b>. Open Publish registration data is stored within Bitcoin transactions. What's important is the useful qualities <i>about</i> the digital media that is being registered, not the digital media itself. The name, the file size, and most importantly, the digital fingerprint that represents the data that constitutes the digital media is stored within Bitcoin transactions and permanetly embedded in the blockchain. By focusing on the formal representation rather than the media itself, we have entered "into the conceptual universe where capital lives."</p>
 
-      <p><i>(3) Making people accountable.</i> Open Publish transactions and registrations are fully accountable. All transactions aim to be public to give a true market value to Open Publish registered media. "By transforming people with real property interests into accountable individuals, formal property [creates] individuals from masses." The individual is then held accountable for their ownership. They maintain their own Bitcoin wallet private keys, or at least choose to work with someone who maintains their private keys on their behalf. They are then free to be fully autonomous as Open Publish is not controlled by any private entity and can then "explore how to generate surplus value from their own assets".</p>
+      <p><i>(2) Integrating dispersed information into one system.</i> Open Publish registrations and transactions are embedded in a shared, singular and public data store. There is a <b>single consensus, maintained in a decentralized manner</b>, built directly into the mechanics of the Bitcoin blockchain. With this unified formal property system "where property information is standardized and universally available, what owners can do with their assets benefits from the collective imagination of a larger network of people."</p>
 
-      <p><i>(4) Making assets fungible.</i> Open Publish allows for the ownership of digital media assets to be transfered in whole or divided in to parts. By default an Open Publish asset is given 100,000,000 units, allowing for individuals to split "their assets into shares, each of which can be owned by different persons, with different rights, to carry out different functions." This enables "owners and entrepreneurs to simulate hypothetical situations in order to explore other profitable uses of their assets."</p>
+      <p><i>(3) Making people accountable.</i> Open Publish transactions and registrations are fully accountable. All transactions aim to be public to give a true market value to Open Publish registered media. "By transforming people with real property interests into accountable individuals, formal property [creates] individuals from masses." <b>The individual is then held accountable for their ownership</b>. They maintain their own Bitcoin wallet private keys, or at least choose to work with someone who maintains their private keys on their behalf. They are then free to be fully autonomous as Open Publish is not controlled by any private entity and can then "explore how to generate surplus value from their own assets".</p>
 
-      <p><i>(5) Networking people.</i> Open Publish is built directly on top of native Bitcoin transactions, which are propagated across the entire network and embedded in each copy of a shared ledger. This allows for anyone to participate in both the Bitcoin and the Open Publish network without permission. All of the "information on the history of assets and owners [is] easily accessible" on both the Bitcoin blockchain and the Open Publish registry. This allows for "radically improved ... flow of communications about assets and their potential", at least compared with current copyright systems and opaque marketplaces for digital media assets like Facebook or Twitter. "This information and the existence of integrated law make risk more manageable by spreading it through insurance-type devices as well as by pooling property to secure debts."</p>
+      <p><i>(4) Making assets fungible.</i> Open Publish allows for the ownership of digital media assets to be transfered in whole or divided in to parts. By default an Open Publish asset is given 100,000,000 units, allowing for individuals <b>to split "their assets into shares, each of which can be owned by different persons</b>, with different rights, to carry out different functions." This enables "owners and entrepreneurs to simulate hypothetical situations in order to explore other profitable uses of their assets."</p>
 
-      <p><i>(6) Protecting transactions.</i> Open Publish registrations and transactions inherit all of the protections and security of the Bitcoin Blockchain. When a Bitcoin transaction is mined it is protected by a cryptographic system that is incredibly secure. "Security is principally focused on producing trust in transactions so that people can more easily make their assets lead a parallel life as capital."</p>
+      <p><i>(5) Networking people.</i> Open Publish is built directly on top of native Bitcoin transactions, which are propagated across the entire network and embedded in each copy of a shared ledger. This <b>allows for anyone to participate</b> in both the Bitcoin and the Open Publish network <b>without permission</b>. All of the "information on the history of assets and owners [is] easily accessible" on both the Bitcoin blockchain and the Open Publish registry. This allows for "radically improved ... flow of communications about assets and their potential", at least compared with current copyright systems and opaque marketplaces for digital media assets like Facebook or Twitter.</p>
+
+      <p><i>(6) Protecting transactions.</i> Open Publish registrations and transactions inherit all of the protections and security of the Bitcoin Blockchain. When a Bitcoin transaction is mined it is <b>protected by a cryptographic system that is incredibly secure</b>. "Security is principally focused on producing trust in transactions so that people can more easily make their assets lead a parallel life as capital."</p>
 
       <hr />
+
+      <h3><Label bsStyle='warning'>The Fun Stuff!!</Label></h3>
+
+      <a name='fun-stuff' />
 
       <p>Imagine there's a man named Stephen Foster who wrote a song called Camptown Races.</p>
 
