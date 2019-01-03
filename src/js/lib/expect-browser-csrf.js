84b7a8b19c1b0f70acfab51667c113446eb0cfa2
William Cotton <williamcotton@gmail.com>
2019-01-03 12:09:47 -0600

    remove old site

--- src/js/lib/expect-browser-csrf.js
+++
@@ -1,7 +0,0 @@
-// this should create a <Form /> react component that is pre-populated with the CSRF
-module.exports = function (options) {
-  return function (req, res, contentProps, rootProps, next) {
-    contentProps.csrf = req.csrf
-    next()
-  }
-}
