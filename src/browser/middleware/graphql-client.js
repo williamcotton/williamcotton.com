8d8917f8e8864977091627b3afcc5ce7d75197ec
William Cotton <williamcotton@gmail.com>
2019-04-06 12:01:42 -0500

    only cache non-mutation queries

--- src/browser/middleware/graphql-client.js
+++ src/browser/middleware/graphql-client.js
@@ -2,6 +2,7 @@ const localQueryCache = {};
 
 module.exports = ({ fetch, queryCache, route, cacheKey }) => (req, res, next) => {
   req.q = async (query, variables) => {
+    const isMutation = /^mutation/.test(query);
     const key = cacheKey(query, variables);
     const cachedResponse = Object.assign(queryCache, localQueryCache)[key];
     const fetchResponse = async () => {
@@ -16,7 +17,7 @@ module.exports = ({ fetch, queryCache, route, cacheKey }) => (req, res, next) =>
       return response.json();
     };
     const response = cachedResponse || (await fetchResponse());
-    localQueryCache[key] = response;
+    if (!isMutation) localQueryCache[key] = response;
     const { data, errors } = response;
     if (errors) {
       throw new Error(errors[0].message);
--- src/server/middleware/graphql-client.js
+++ src/server/middleware/graphql-client.js
@@ -2,9 +2,10 @@ const { graphql } = require('graphql');
 
 module.exports = ({ schema, rootValue, cacheKey }) => (req, res, next) => {
   req.q = async (query, variables) => {
+    const isMutation = /^mutation/.test(query);
     const key = cacheKey(query, variables);
     const response = await graphql(schema, query, rootValue, null, variables);
-    res.cacheQuery(key, response);
+    if (!isMutation) res.cacheQuery(key, response);
     const { data, errors } = response;
     if (errors) {
       throw new Error(errors[0].message);
